9dc0b10c75b83f8e1183b22000e2b4cd64f97c99
knightsarai <knight.sarai.dev@gmail.com>
2024-05-03 21:01:11 +0300

    Add switch language binding

--- pop-os-conf.sh
+++ pop-os-conf.sh
@@ -2,34 +2,7 @@
 
 # Install flat pack
 flatpak install flathub com.mattjakeman.ExtensionManager
-## Then Manually install workspace bar
 
-# Work spaces
-# dconf write /org/gnome/mutter/dynamic-workspaces false
-# dconf write /org/gnome/desktop/wm/preferences/num-workspaces 8
-#
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Super>1']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>2']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>3']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Super>4']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>5']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>6']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>7']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Super>8']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Super>9']"
-# dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-10 "['<Super>0']"
-#
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-1  "['<Super><Shift>1']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-2  "['<Super><Shift>2']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-3  "['<Super><Shift>3']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-4  "['<Super><Shift>4']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-5  "['<Super><Shift>5']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-6  "['<Super><Shift>6']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-7  "['<Super><Shift>7']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-8  "['<Super><Shift>8']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-9  "['<Super><Shift>9']"
-# dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-10 "['<Super><Shift>0']"
-#
 # Alternative way to set workspaces
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
@@ -74,3 +47,5 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'gnome-screensaver-command -l'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Super>Escape'
 gsettings set org.gnome.settings-daemon.plugins.media-keys suspend "['<Super><Shift>s']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Super><Control>BackSpace']"
